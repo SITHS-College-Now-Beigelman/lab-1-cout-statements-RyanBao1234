73145277f22bf4e5a17154e1c4b7b3c258290a7f
RyanBao1234 <ryanb272@nycstudents.net>
2024-09-10 17:17:24 -0400

    Bao_Ryan_Lab_1
    Lab fixed with code 0

--- Bao_Ryan_Lab_1.cpp
+++ Bao_Ryan_Lab_1.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 
-using namespace std;
+using namespace std;                                                                                                                                                          
 
 int main()                                                                                                             //initialize function
 {
@@ -17,20 +17,21 @@ int main()                                                                      
    cout << "#                                                                                    #" << endl;
    cout << "######################################################################################" << endl;
 
+   return 0;
 }
 
 
 /*
 [Running] cd "c:\Users\Ryan.Bao23\Documents\GitHub\lab-1-cout-statements-RyanBao1234\" && g++ Bao_Ryan_Lab_1.cpp -o Bao_Ryan_Lab_1 && "c:\Users\Ryan.Bao23\Documents\GitHub\lab-1-cout-statements-RyanBao1234\"Bao_Ryan_Lab_1
-####################################################################################
-#                                                                                  #
-# Ways to access the Task Manager on your Windows computer:                        # 
-#           Press the key combination Ctrl + Shift + Escape                        #
-#          Press the key combination Ctrl + Alt + Delete and select “Task Manager” #
-#           Type “Task Manager” in the Windows Start menu search                   #
-#                                                                                  #
-####################################################################################
+######################################################################################
+#                                                                                    #
+# Ways to access the Task Manager on your Windows computer:                          # 
+#            Press the key combination Ctrl + Shift + Escape                         #
+#            Press the key combination Ctrl + Alt + Delete and select “Task Manager” #
+#            Type “Task Manager” in the Windows Start menu search                    #
+#                                                                                    #
+######################################################################################
 
-[Done] exited with code=0 in 1.458 seconds
+[Done] exited with code=0 in 1.515 seconds
 
 */
