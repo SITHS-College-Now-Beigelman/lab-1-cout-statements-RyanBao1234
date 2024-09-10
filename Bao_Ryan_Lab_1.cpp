c400bc403cb334b366eb98fc9eef1084678a8604
RyanBao1234 <ryanb272@nycstudents.net>
2024-09-10 16:55:13 -0400

    Bao_Ryan_Lab_1
    Lab_1

---
+++ Bao_Ryan_Lab_1.cpp
@@ -0,0 +1,36 @@
+//Ryan Bao
+//In-Class Lab 1: cout Statements
+//09/10/2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()                                                                                                             //initialize function
+{
+   cout << "######################################################################################" << endl;             //print statements
+   cout << "#                                                                                    #" << endl;
+   cout << "# Ways to access the Task Manager on your Windows computer:                          # " << endl;
+   cout << "#            Press the key combination Ctrl + Shift + Escape                         #" << endl;
+   cout << "#            Press the key combination Ctrl + Alt + Delete and select “Task Manager” #" << endl;
+   cout << "#            Type “Task Manager” in the Windows Start menu search                    #" << endl;
+   cout << "#                                                                                    #" << endl;
+   cout << "######################################################################################" << endl;
+
+}
+
+
+/*
+[Running] cd "c:\Users\Ryan.Bao23\Documents\GitHub\lab-1-cout-statements-RyanBao1234\" && g++ Bao_Ryan_Lab_1.cpp -o Bao_Ryan_Lab_1 && "c:\Users\Ryan.Bao23\Documents\GitHub\lab-1-cout-statements-RyanBao1234\"Bao_Ryan_Lab_1
+####################################################################################
+#                                                                                  #
+# Ways to access the Task Manager on your Windows computer:                        # 
+#           Press the key combination Ctrl + Shift + Escape                        #
+#          Press the key combination Ctrl + Alt + Delete and select “Task Manager” #
+#           Type “Task Manager” in the Windows Start menu search                   #
+#                                                                                  #
+####################################################################################
+
+[Done] exited with code=0 in 1.458 seconds
+
+*/
